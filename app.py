e46ec93168c9189d3ea1770a9e6c631eadc040b9
HENRY <codexharry11@gmail.com>
2025-11-29 21:35:22 +0000

    quiz

--- app.py
+++ app.py
@@ -2,15 +2,29 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-questions = [
-    {"question": "What was the independence year of Ghana?", "answer": "1957"},
-    {"question": "What is the specific name given to the infant of a dog?", "answer": "puppy"},
-    {"question": "A global network of interconnected devices is known as what?", "answer": "internet"},
-    {"question": "What is the full meaning of CPU?", "answer": "central processing unit"}
+questions =[
+{
+"question": "What was the independence year of Ghana?",
+"options":["1959", "1966", "1957", "1965"],
+"answer": "1957"}
+{
+"question": "What is the specific name given to the infant of a dog?", 
+"options": ["cub", "puppy", "slay", "kitten"],
+"answer": "puppy"}
+{
+"question": "A global network of interconnected devices is known as what?",
+"options": ["internet", "network", "world wide web", "transfer"],
+"answer": "internet"}
+{
+"question": "What country is the largest in Africa? ",
+"options": ["senegal", "ghana", "cote-d'ivore", "nigeria"],
+"answer": "nigeria"}
+{
+"question": "Full meaning of CPU?",
+"options": ["Central Process Unit", "Central Processing Unit", "Computer Processing Unit", "Central Power Unit"],
+"answer": "Central Processing Unit"
+}
 ]
-
-score_no = 0
-ques_no = 0
 
 
 @app.route("/")
@@ -20,9 +34,10 @@ def home():      # This is you URL for your welcome screen
 
 @app.route("/start")
 def start():  # This run at start
-    global score_no, ques_no
+    global score_no, ques_no, correct
     score_no = 0
     ques_no = 0
+    correct = 0
     return redirect(url_for("question"))
 
 
@@ -30,24 +45,25 @@ def start():  # This run at start
 def question():
     global score_no, ques_no
     if request.method == "POST":
-        user_ans = request.form["answer"].strip().lower()
-        corr_ans = questions[ques_no]["answer"].lower()
+        user_opt = request.form["answer"].strip().lower()
+        corr_opt = questions[ques_no]["answer"].lower()
 
-        if user_ans == corr_ans:
+        if user_opt == corr_opt:
             score_no += 1
+            correct += 1
         ques_no += 1
         if ques_no >= len(questions):
             return redirect(url_for("result"))
 
     current = questions[ques_no]
-    return render_template("question.html", question=current["question"], ques_no = ques_no +1)
+    return render_template("question.html", question=current["question"], options= current["options"], ques_no = ques_no +1)
 
 
 @app.route("/result")
 def result():
-    global score_no, ques_no
+    global score_no, ques_no, correct
     total_per = round(score_no/len(questions)*100, 2)
-    return render_template("result.html", total=len(questions), percent=total_per, score=score_no * 2, num=ques_no)
+    return render_template("result.html", total=len(questions), percent=total_per, correct= correct, score=score_no * 2, num=ques_no)
 
 
 if __name__ == "__main__":
